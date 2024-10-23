59dc318b735ad7631857440d7794c225f899589d
jessicagel <jessicaafisio@gmail.com>
2024-10-22 22:10:33 -0300

    grupo de estudo

---
+++ indez.js
@@ -0,0 +1,75 @@
+// let numero = prompt('Digite 1 números')
+// let total = 0
+
+// for (let i = 1; i <= numero; i++){
+// total += i
+
+// // total = 0 + 1
+// // tota = 1 + 2
+// // tital = 3 + 3
+
+// }
+
+// document.write(`a soma total é ${total}`) 
+
+// let numero = prompt ('Digite um número')
+// let numeroImpar = []
+
+// for (let i = 1; i <= numero; i++){
+
+// if (i % 2 != 0){
+//     numeroImpar.push(i)
+// }
+// }   
+// document.write (`O numeros impares são ${numeroImpar}`)
+
+
+// let numero = prompt ('Digite um número')
+// let numeroFatorial = 1
+
+// for (let i = 1; i <= numero; i++){
+//     numeroFatorial *= i;
+    
+// }
+// document.write(`O fatorial de ${numero} é ${numeroFatorial}`);
+
+
+// let numero = prompt ('Digite um número');
+// let divisores = 0;
+
+// for (let i = 1; i <= numero; i++){
+//     if (numero % i == 0){
+//         divisores ++;
+//     }
+// }
+//     if (divisores == 2){
+//     document.write (`0 ${numero} é primo`)
+//     }
+//     else if (divisores >2) {
+//         document.write (`O ${numero} não é primo`)
+//     }
+
+
+// let numero = prompt ('Digite um número')
+
+// for (let i = 1; i <= 10; i++){
+//     document.write(`${numero} x ${i} = ${numero * i} <br>`)
+// }
+
+// let numero1 = parseFloat (prompt ('Digite 1 número'))
+// let numero2 = parseFloat (prompt ('Digite 1 número'))
+// let numero3 = parseFloat (prompt ('Digite 1 número'))
+// let numero4 = parseFloat (prompt ('Digite 1 número'))
+// let numero5 = parseFloat (prompt ('Digite 1 número'))
+
+
+// let numeros = [numero1, numero2, numero3, numero4, numero5]
+// let soma = 0
+
+
+// for (let i = 0; i < 5; i++) {
+//     soma += numeros[i]; 
+// }
+// let media = soma / 5;
+// document.write(`A média dos números é: ${media}`);
+
